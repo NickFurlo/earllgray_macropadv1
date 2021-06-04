e59ee78474f6bd53ede918e16c784a43b38d1b41
Nick furlo <furlopunk@gmail.com>
2021-06-03 20:35:32 -0400

    Final version
    row and columns were mapped to the wrong io ports. Adjusted my wiring to put space in the middle column.

--- earllgray_macropadv1/config.h
+++ earllgray_macropadv1/config.h
@@ -6,16 +6,19 @@
 #define VENDOR_ID       0xEEEE
 #define PRODUCT_ID      0x2019
 #define DEVICE_VER		0x0001
-#define MANUFACTURER    Nick Furlo
-#define PRODUCT         earllgray-macropadv1
+#define MANUFACTURER    NickFurlo
+#define PRODUCT         earllgray_macropadv1
 
 /* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 4
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 5
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS { D1, D0, D4, C6, D7, B3 }
-#define MATRIX_COL_PINS { E6, B4, B5, B1 }
+/*#define MATRIX_ROW_PINS { E6, B4, B5, B1 }
+#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, B3 }
+These pins must be in the same order that you used to define the keymap {row 1, row2, ...}*/
+#define MATRIX_ROW_PINS { E6, B4, B5, B1 }
+#define MATRIX_COL_PINS { C6, D4, B3, D0, D1 }
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
